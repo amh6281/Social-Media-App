101070a21a4a3eb94c398dc14feef21b526ccf8c
amh6281 <amh6281@naver.com>
2022-11-02 16:00:22 +0900

    feat: private routes 적용 2022.11.02
    react-router의 private routes를 적용하여 로그인을 하지않으면 Login페이지로 이동한다.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 
 function App() {
+  const currentUser = false;
+
   const Layout = () => {
     return (
       <div>
@@ -26,10 +28,23 @@ function App() {
       </div>
     );
   };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
       children: [
         {
           path: "/",

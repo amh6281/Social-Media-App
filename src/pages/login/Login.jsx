f4a6cd6f4caa497fdff67d11953caf13ab1d82b2
amh6281 <amh6281@naver.com>
2022-11-02 14:08:29 +0900

    feat: 회원가입페이지 구축 2022.11.02

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -19,8 +19,8 @@ const Login = () => {
         <div className="right">
           <h1>로그인</h1>
           <form>
-            <input type="text" placeholder="ID" />
-            <input type="password" placeholder="PW" />
+            <input type="text" placeholder="아이디" />
+            <input type="password" placeholder="비밀번호" />
             <button>로그인</button>
           </form>
         </div>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,7 +2,33 @@ import React from "react";
 import "./register.scss";
 
 const Register = () => {
-  return <div className="register">Register</div>;
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Hello Social</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+            voluptatum itaque, sapiente officiis odio provident quam aspernatur
+            quae accusamus laudantium quos! Fugit nam itaque beatae? Molestiae
+            eum nemo quam mollitia.
+          </p>
+          <span>계정이 있으신가요?</span>
+          <button>로그인</button>
+        </div>
+        <div className="right">
+          <h1>회원가입</h1>
+          <form>
+            <input type="text" placeholder="아이디" />
+            <input type="text" placeholder="이메일" />
+            <input type="password" placeholder="비밀번호" />
+            <input type="text" placeholder="이름" />
+            <button>회원가입</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;

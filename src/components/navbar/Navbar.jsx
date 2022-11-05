e2638e0fd1d77de784e61aedffed60658427f4d2
amh6281 <amh6281@naver.com>
2022-11-05 12:40:21 +0900

    feat: Navbar UI 구현 2022.11.05

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,44 @@
 import React from "react";
 import "./navbar.scss";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <span>HelloWorld</span>
+        </Link>
+        <HomeOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="search" />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img
+            src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+            alt=""
+          />
+          <span>가나다</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;

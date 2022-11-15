00d32da3819e43f2803dc77d9c10333ce16113a1
amh6281 <amh6281@naver.com>
2022-11-15 09:57:49 +0900

    feat: Rightbar 구현 2022.11.15
    추천 친구, 최근 활동 및 활동중인 친구를 바탕으로 Rightbar를 구현한다.

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,7 +2,205 @@ import React from "react";
 import "./rightbar.scss";
 
 const Rightbar = () => {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="container">
+        <div className="item">
+          <span>추천 친구</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <span>홍길동</span>
+            </div>
+            <div className="buttons">
+              <button>친구 추가</button>
+              <button>삭제</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <span>홍길동</span>
+            </div>
+            <div className="buttons">
+              <button>친구 추가</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>최근 활동</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <p>
+                <span>홍길동</span> 배경 사진을 변경하였습니다.
+              </p>
+            </div>
+            <span>1분전</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <p>
+                <span>홍길동</span> 배경 사진을 변경하였습니다.
+              </p>
+            </div>
+            <span>1분전</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <p>
+                <span>홍길동</span> 배경 사진을 변경하였습니다.
+              </p>
+            </div>
+            <span>1분전</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <p>
+                <span>홍길동</span> 배경 사진을 변경하였습니다.
+              </p>
+            </div>
+            <span>1분전</span>
+          </div>
+        </div>
+        <div className="item">
+          <span>활동중인 친구</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <div className="online" />
+              <span>홍길동</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <div className="online" />
+              <span>홍길동</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <div className="online" />
+              <span>홍길동</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <div className="online" />
+              <span>홍길동</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <div className="online" />
+              <span>홍길동</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <div className="online" />
+              <span>홍길동</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <div className="online" />
+              <span>홍길동</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <div className="online" />
+              <span>홍길동</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <div className="online" />
+              <span>홍길동</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <div className="online" />
+              <span>홍길동</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                alt=""
+              />
+              <div className="online" />
+              <span>홍길동</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Rightbar;

ed442d43764ab39f50267db6a4a86f231e660657
amh6281 <amh6281@naver.com>
2022-11-23 12:52:32 +0900

    feat: Posts 컴포넌트 UI 구현 2022.11.23

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./post.scss";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
+import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Link } from "react-router-dom";
+
+const Post = ({ post }) => {
+  //TEMPORARY
+  const liked = false;
+  return (
+    <div className="post">
+      <div className="container">
+        <div className="user">
+          <div className="userInfo">
+            <img src={post.profilePic} alt="" />
+            <div className="details">
+              <Link
+                to={`/profile/${post.userId}`}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <span className="name">{post.name}</span>
+              </Link>
+              <span className="date">1분 전</span>
+            </div>
+          </div>
+          <MoreHorizIcon />
+        </div>
+        <div className="content">
+          <p>{post.desc}</p>
+          <img src={post.img} alt="" />
+        </div>
+        <div className="info">
+          <div className="item">
+            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
+            좋아요 12개
+          </div>
+          <div className="item">
+            <TextsmsOutlinedIcon />
+            댓글 5개
+          </div>
+          <div className="item">
+            <ShareOutlinedIcon />
+            공유 13회
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,8 +1,35 @@
 import React from "react";
+import Post from "../post/Post";
 import "./posts.scss";
 
 const Posts = () => {
-  return <div className="posts">Posts</div>;
+  //TEMPORARY
+  const posts = [
+    {
+      id: 1,
+      name: "김철수",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: 2,
+      name: "김철수",
+      userId: 2,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, mollitia animi, inventore pariatur, eveniet possimus harum eum facilis eligendi molestias odio expedita dignissimos ut esse hic minus minima nobis vero.",
+    },
+  ];
+  return (
+    <div className="posts">
+      {posts.map((post) => (
+        <Post post={post} key={post.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Posts;

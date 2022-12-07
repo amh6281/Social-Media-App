1dd02fbb76aee56dc0f4643172d5e679920a0d3d
amh6281 <amh6281@naver.com>
2022-12-07 14:35:09 +0900

    auth, user, post, comment, like controller 생성 2022.12.07
    필수 요소인 auth, user, post, comment, like controller를 생성한다.

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import Express from "express";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+
 const app = Express();
+
+app.use("/api/users", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/users", postRoutes);
+app.use("/api/users", commentRoutes);
+app.use("/api/users", likeRoutes);
 
 app.listen(8800, () => {
   console.log("API working!!");

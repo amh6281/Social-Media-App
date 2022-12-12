61f2b50607eae9f11306eca46c1d9502043346b2
amh6281 <amh6281@naver.com>
2022-12-12 11:03:39 +0900

    React query fetching data 2022.12.12

--- client/src/components/leftbar/Leftbar.jsx
+++ client/src/components/leftbar/Leftbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./leftbar.scss";
 import Friends from "../../assets/1.png";
 import Groups from "../../assets/2.png";
@@ -13,18 +13,18 @@ import Messages from "../../assets/10.png";
 import Tutorials from "../../assets/11.png";
 import Courses from "../../assets/12.png";
 import Fund from "../../assets/13.png";
+import { AuthContext } from "../../context/authContext";
 
 const Leftbar = () => {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <div className="leftbar">
       <div className="container">
         <div className="menu">
           <div className="user">
-            <img
-              src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
-              alt=""
-            />
-            <span>가나다</span>
+            <img src={currentUser.profilePic} alt="" />
+            <span>{currentUser.name}</span>
           </div>
           <div className="item">
             <img src={Friends} alt="" />
--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,31 +1,18 @@
 import React from "react";
 import Post from "../post/Post";
 import "./posts.scss";
+import { useQuery } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
 
 const Posts = () => {
-  //TEMPORARY
-  const posts = [
-    {
-      id: 1,
-      name: "김철수",
-      userId: 1,
-      profilePic:
-        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    },
-    {
-      id: 2,
-      name: "김철수",
-      userId: 2,
-      profilePic:
-        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, mollitia animi, inventore pariatur, eveniet possimus harum eum facilis eligendi molestias odio expedita dignissimos ut esse hic minus minima nobis vero.",
-    },
-  ];
+  const { isLoading, error, data } = useQuery(["posts"], () =>
+    makeRequest.get("/posts").then((res) => {
+      return res.data;
+    })
+  );
   return (
     <div className="posts">
-      {posts.map((post) => (
+      {data.map((post) => (
         <Post post={post} key={post.id} />
       ))}
     </div>
